7139bf2a1e19234493ef072bd2c84fe79532dc51
Dale <patas.devel@gmail.com>
2021-11-27 01:05:19 +0100

    A few changes in machine create parameters

--- pyswitch/mother_db_add.py
+++ pyswitch/mother_db_add.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python
+
+# POPIS: Otestovana na mother prepro stroji
+
 import os
+import sys
+sys.path.append('/root/mother/')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mother.settings")
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myswitch.settings")
+from django.db import models
+from  mother.machines.models import Machine
 
-# your imports, e.g. Django models
-from myswitch.models import Machine
+#print(Machine._meta.get_all_field_names())
+os.system('clear')
 
-# From now onwards start your script..
-m = Machine()
-m.name = 'Test'
-m.save()
+m = Machine.objects.create(name='a-server6', serial_number='ABCEFGHIJK', project_id_id=1, inventory='testovaci6', cpu=4, ram=16, os='d10', HeliosID='123123', qr_code='12312346', type_id=132, state_id=1, site_id=1)
